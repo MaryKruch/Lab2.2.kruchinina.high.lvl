60537f280115c4577e986b3c8016b56cbe2be173
NIK <NIK@DESKTOP-MH7JFSC>
2023-10-14 21:26:59 +0200

    Add project files.

---
+++ Lab2.2.kruchinina.high.lvl/Program.cs
@@ -0,0 +1,3 @@
+﻿Console.Write("Введите x:"); double x = double.Parse(Console.ReadLine());
+Console.Write("Введите y:"); double y = double.Parse(Console.ReadLine());
+Console.WriteLine((x >= -2 && x <= 2 && y > 0 && y <= 2 && y >= x + 2 && y > -x + 2));
